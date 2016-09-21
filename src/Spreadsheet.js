73f0701d448cc593d7d75aa169ef8015eaca0ab4
Roger Chapman <rogchap@gmail.com>
2016-09-21 12:48:01 +1000

    Parse raw data into Map

--- src/Spreadsheet.js
+++ src/Spreadsheet.js
@@ -1,3 +1,10 @@
+/* @flow */
+
+import Cell from './Cell';
+
+type DataCol = string;
+type DataRow = Array<DataCol>;
+type DataTable = Array<DataRow>;
 
 /**
  * Represents a Spreadsheet, as columns and rows
@@ -6,5 +13,35 @@
  * @class Spreadsheet
  */
 export default class Spreadsheet {
+  _rawData: DataTable;
+  _dataMap: {[key: string]: Cell};
 
+  /**
+   * Creates an instance of Spreadsheet.
+   * data is expected in the format: [ [Col1Val, Col2Val, ...], [Col1Val, Col1Val, ...] ]
+   *
+   * @param {DataTable} data
+   *
+   * @memberOf Spreadsheet
+   */
+  constructor(data: DataTable) {
+    this._rawData = data;
+    this._dataMap = {};
+    this._parseData();
+  }
+
+  _parseData() {
+
+    // Convert the raw data into a Map so that we can ref each cell
+    this._rawData.forEach((row, rowIdx) => {
+      row.forEach((col, colIdx) => {
+        const cell = new Cell(colIdx, rowIdx, col);
+        this._dataMap[cell.id] = cell;
+      });
+    });
+  }
+
+  compute() {
+
+  }
 }
--- test/spreadsheet.spec.js
+++ test/spreadsheet.spec.js
@@ -1,10 +1,54 @@
-import chai from 'chai';
-chai.should();
+import { expect } from 'chai';
+import Spreadsheet from '../src/Spreadsheet';
+import Cell from '../src/Cell';
 
 describe('Spreadsheet', () => {
+  let spreadsheet;
 
-  it('is not real', () => {
-    false.should.not.be.ok;
+  // const dataTable = [
+  //   [ 'b1 b2 +', '2 b2 3 *', '3', '+' ],
+  //   [ 'a1     ', '5         ','   ', '7 2 /' ],
+  //   [ 'c2 3 * ', '1 2       ', '   ', '5 1 2 + 4 * + 3' ­],
+  // ];
+
+  // beforeEach(() => {
+  //   spreadsheet = new Spreadsheet(dataTable);
+  // });
+
+  describe('parsing data', () => {
+    const dt = [
+      ['a', 'b', 'c'],
+      ['1', '2', '3'],
+    ];
+
+    beforeEach(() => {
+      spreadsheet = new Spreadsheet(dt);
+    });
+
+    describe('#_dataMap', () => {
+      it('does exist', () => {
+        expect(spreadsheet._dataMap, 'spreadsheet._dataMap').to.exist;
+      });
+
+      it('should have a correct key/value', () => {
+        expect(spreadsheet._dataMap).to.have.property('A1');
+        expect(spreadsheet._dataMap).to.have.property('B1');
+        expect(spreadsheet._dataMap).to.have.property('C1');
+        expect(spreadsheet._dataMap).to.have.property('A2');
+        expect(spreadsheet._dataMap).to.have.property('B2');
+        expect(spreadsheet._dataMap).to.have.property('C2');
+      });
+
+      it('should have a Cell as value', () => {
+        expect(spreadsheet._dataMap['A1']).to.be.an.instanceof(Cell);
+      });
+
+      it('should have key match Cell ID', () => {
+        expect(spreadsheet._dataMap['A1'].id).to.equal('A1');
+      });
+
+    });
   });
+
 
 })
